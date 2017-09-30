324a4ad085f55ee47db74fbfc316794c14bab15b
haisongliang <haisongliang@gmail.com>
2017-09-30 19:16:54 +0800

    添加项目文件。

---
+++ qn_unpack/qn_unpack.cpp
@@ -0,0 +1,286 @@
+﻿// qn_unpack.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <windows.h>
+#include "bzlib.h"
+#pragma comment(lib, "libbz2.lib")
+
+typedef size_t uint32;
+typedef size_t uint;
+typedef unsigned char uint8;
+typedef int int32;
+
+const uint8* GetPackKey()
+{
+	//异或加密的KEY
+	static const uint8 ls_PackKey[16] =
+	{ 0xEB, 0xC2,0xDC,0xC3,0xC4,0xB5,0xD2,0xCA,
+		0xF7,0xD7,0xA4,0xB9,0xF0,0xBB,0xD7,0xC0 };
+
+	return ls_PackKey;
+}
+
+void XorBuf(char* buf)
+{
+	const uint8* key = GetPackKey();
+
+	for (int i = 0; i < 1024; i += 16)
+		for (int j = 0; j < 16; j++)
+			buf[i + j] = buf[i + j] ^ key[j];
+}
+
+inline static void BufferXor(char * buffer, uint32 uPosition, size_t uBytes)
+{
+	const uint8* szPackKey = GetPackKey();
+
+	for (unsigned i = 0; i < uBytes; i++)
+		buffer[i] = buffer[i] ^ szPackKey[(uPosition + i) & 0xf];
+}
+
+// 为了适应将字符串转化为唯一的数字ID，调整字符串
+// 如果字符大写，就转化为小写，将'\\'转化为'/'
+static char* StringAdjust(char* p)
+{
+	for (int i = 0; p[i]; i++)
+	{
+		if (p[i] >= 'A' && p[i] <= 'Z')
+			p[i] += 'a' - 'A';
+		else if (p[i] == '/')
+			p[i] = '\\';
+	}
+	return p;
+}
+
+#define mix(a,b,c) \
+			{ \
+				a -= b; a -= c; a ^= (c>>13); \
+				b -= c; b -= a; b ^= (a<<8); \
+				c -= a; c -= b; c ^= (b>>13); \
+				a -= b; a -= c; a ^= (c>>12);  \
+				b -= c; b -= a; b ^= (a<<16); \
+				c -= a; c -= b; c ^= (b>>5); \
+				a -= b; a -= c; a ^= (c>>3);  \
+				b -= c; b -= a; b ^= (a<<10); \
+				c -= a; c -= b; c ^= (b>>15); \
+			}
+
+inline uint32 hash(
+	register uint8 *k,        /* the key */
+	register uint32  length,   /* the length of the key */
+	register uint32  initval    /* the previous hash, or an arbitrary value */
+)
+{
+	register uint32 a, b, c, len;
+
+	/* Set up the internal state */
+	len = length;
+	a = b = 0x9e3779b9;  /* the golden ratio; an arbitrary value */
+	c = initval;           /* the previous hash value */
+
+						   /*---------------------------------------- handle most of the key */
+	while (len >= 12)
+	{
+		a += (k[0] + ((uint32)k[1] << 8) + ((uint32)k[2] << 16) + ((uint32)k[3] << 24));
+		b += (k[4] + ((uint32)k[5] << 8) + ((uint32)k[6] << 16) + ((uint32)k[7] << 24));
+		c += (k[8] + ((uint32)k[9] << 8) + ((uint32)k[10] << 16) + ((uint32)k[11] << 24));
+		mix(a, b, c);
+		k += 12;
+		len -= 12;
+	}
+
+	/*------------------------------------- handle the last 11 bytes */
+	c += length;
+	switch (len)             /* all the case statements fall through */
+	{
+	case 11:
+		c += ((uint32)k[10] << 24);
+	case 10:
+		c += ((uint32)k[9] << 16);
+	case 9:
+		c += ((uint32)k[8] << 8);
+		/* the first byte of c is reserved for the length */
+	case 8:
+		b += ((uint32)k[7] << 24);
+	case 7:
+		b += ((uint32)k[6] << 16);
+	case 6:
+		b += ((uint32)k[5] << 8);
+	case 5:
+		b += k[4];
+	case 4:
+		a += ((uint32)k[3] << 24);
+	case 3:
+		a += ((uint32)k[2] << 16);
+	case 2:
+		a += ((uint32)k[1] << 8);
+	case 1:
+		a += k[0];
+		/* case 0: nothing left to add */
+	}
+	mix(a, b, c);
+	/*-------------------------------------------- report the result */
+	return c;
+}
+
+uint32 String2ID(const char* str)
+{
+	if (str == NULL)
+		return 0;
+	return hash((uint8*)str, (uint32)strlen(str), 0);
+}
+
+unsigned int FileName2Id(const char *filename)
+{
+	char buffer[256];
+	buffer[255] = 0;
+	strncpy(buffer, filename, 255);
+	return (unsigned int)String2ID(StringAdjust(buffer));
+}
+
+#pragma  pack (push,1) 
+//文件索引
+struct PackFileIndex
+{
+	//ID
+	uint32		uid;
+	//文件偏移
+	uint		offset;
+	//文件尺寸
+	uint		size;
+
+	uint		space;
+	//校验和
+	uint32		check_code;
+
+	PackFileIndex()
+		: uid(0)
+		, offset(0)
+		, size(0)
+		, space(0)
+		, check_code(0)
+	{
+	}
+};
+
+//文件头
+struct PackageHead
+{
+	//ID
+	uint		id;
+	//文件是否使用异或加密
+	uint		bXor;
+	//当前包中文件的数量
+	int			number;
+	//文件索引的偏移
+	uint		offset;
+	//包版本
+	uint32		ver;
+
+	PackageHead()
+		: id('XWDF')
+		, bXor(0)
+		, number(0)
+		, offset(0)
+		, ver(0)
+	{
+	}
+};
+#pragma pack(pop) 
+int main()
+{
+	std::string szFileName = "C:/script/test1/test2_c.ams";
+	// 从源文件路径中去掉包含包名的路径，使用相对路径计算uid
+	std::string temp = "script.pkg";
+	temp = temp.substr(0, temp.size() - 4) + "/";
+
+	//int32  pos  = szFileName.find(temp) + temp.size();
+
+	std::string fullfolder = "/" + temp;
+	int32 pos = szFileName.rfind(fullfolder);
+	if (pos == std::string::npos)
+	{
+		if (szFileName.substr(0, temp.size()) == temp)
+		{
+			pos = temp.size();
+		}
+		else
+		{
+			//cerr << (string("路径") + szFileName + "中不存在" + fullfolder);
+		}
+	}
+	else
+	{
+		pos += fullfolder.size();
+	}
+
+	szFileName = szFileName.substr(pos, szFileName.size());
+
+	uint32 id = FileName2Id(szFileName.c_str());
+	printf("%d\n", id);
+	FILE* pFile = fopen("D:\\script.wdf", "r");
+	PackageHead fileHead;
+	fread(&fileHead, sizeof(PackageHead), 1, pFile);
+	
+	printf("filehead:\nID:%d num:%d offset:%d xor:%d ver:%d\n", fileHead.id, fileHead.number,
+		fileHead.offset, fileHead.bXor, fileHead.ver);
+	PackFileIndex fileIndex;
+	char fileBuffer[1000000];
+	for (size_t i = 0; i < fileHead.number; i++)
+	{
+		fseek(pFile, fileHead.offset + i*sizeof(fileIndex), SEEK_SET);
+		int aaa = fread(&fileIndex, sizeof(fileIndex), 1, pFile);
+		//printf("%d\n",			fileIndex.check_code);
+		//printf("%d\n", ftell(pFile));"%d\t%d\t%d\t%d\t%d\n"
+		//printf("%d\n", fileIndex.uid, fileIndex.offset, fileIndex.size, fileIndex.space,
+		//	fileIndex.check_code);
+
+		//Sleep(1000);
+		//continue;
+		if (fileIndex.uid != id)
+		{
+			//continue;
+		}
+		
+		char fileName[255] = "";
+		_itoa(fileIndex.uid, fileName, 10);
+
+		{
+			memset(fileBuffer, 0, sizeof(fileBuffer));
+			fseek(pFile, fileIndex.offset, SEEK_SET);
+			fread(fileBuffer, 1, fileIndex.size, pFile);
+			//XorBuf(fileBuffer);
+			char decompress[200000] = "";
+			unsigned int len = sizeof(decompress);
+			int aaaaaa = BZ2_bzBuffToBuffDecompress(decompress, &len, fileBuffer,
+				fileIndex.size, 0, 3);
+			if (aaaaaa == 0)
+			{
+				FILE* unPackFile = fopen(fileName, "w+");
+				if (unPackFile)
+				//XorBuf(decompress);
+				BufferXor(decompress, 0, len);
+				//if (len == fileIndex.space)
+				//{
+				//	printf("!!!!!!!!!!!!!\n");
+				//}
+				//printf("1\n");
+				fwrite(decompress, 1, len, unPackFile);
+				fclose(unPackFile);
+			}
+			else {
+				printf("0\n");
+				//XorBuf(fileBuffer);
+				//fwrite(fileBuffer, 1, fileIndex.size, unPackFile);
+			}					
+			
+			//delete[] fileBuffer;
+		}
+	}
+	fclose(pFile);
+	return 0;
+}
+
---
+++ xor/xor.cpp
@@ -0,0 +1,279 @@
+﻿//用一个密钥(字符串)加密另一个字符串,密钥是循环使用,加密方式为异或.现在要从密文中破解密钥来实现破解.
+//这里假设密钥是由小写字母构成的组合,其他情况也和这类似
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<string>
+
+using namespace std;
+
+int my_strlen(const char * str) {
+
+	const char *p = str;
+	while (*p++);
+	return((int)(p - str - 1));
+}
+
+int function(int c1, int c2) {
+	return c1^c2;
+}
+
+void JiaMi(const char* src, char * des, const char *key) {
+	ofstream out("JiaMi.txt");
+	int lengthOfKey = my_strlen(key);
+	for (int i = 0; i<my_strlen(src); i++) {
+		*(des + i) = char(function(*(src + i), *(key + i%lengthOfKey)));
+		out << (int)*(des + i) << "  ";
+
+	}
+	out.close();
+	cout << endl;
+}
+
+void JiaMi2(const char* src, char * des, const char *key) {
+	ofstream out("JiaMi.txt");
+
+	for (int i = 0; i < my_strlen(src); i++) {
+		out << (int)*(src + i) << "  ";
+	}
+	out.close();
+	cout << endl;
+}
+
+void JieMi(const char* src, char * des, const char *key, int Length) {//将src和key或运算
+	for (int i = 0; i<Length; i++) {
+		*(des + i) = char(function(*(src + i), *(key + i%my_strlen(key))));
+	}
+	cout << endl;
+}
+
+
+float ratio(int* src, int length) {//求重合指数率的函数
+	int TotalOfEquals = 0;
+	int total = 0;
+	for (int i = 0; i<length; i++) {
+		for (int j = i + 1; j<length; j++) {
+			total++;
+			if (*(src + i) == *(src + j)) {
+				TotalOfEquals++;
+			}
+		}
+	}
+	return float((float)TotalOfEquals / (float)total);
+}
+
+void JieMiWithoutKey(int* src, char * des, int Length) {//src是暗文,des存放目标明文,Length是暗文长度
+	ofstream jiemi("jiemi.txt");
+
+	//计算一下暗文的重合指数
+	int finalNo = 0;//密码长度
+	int TotalOfEquals = 0;
+	int total = 0;
+	for (int i = 0; i<Length; i++) {
+		for (int j = i + 1; j<Length; j++) {
+			total++;
+			if (*(src + i) == *(src + j)) {
+				TotalOfEquals++;
+			}
+		}
+	}
+
+	cout << "TotalOfEquals=" << TotalOfEquals << endl;
+	cout << "total=" << total << endl;
+	cout << "total总数为:" << Length*Length / 2 << endl;
+	jiemi << "TotalOfEquals=" << TotalOfEquals << endl;
+	jiemi << "total=" << total << endl;
+	jiemi << "total总数为:" << Length*Length / 2 << endl;
+	cout << "全文重合指数为: " << (float)TotalOfEquals / (float)total << endl;
+	jiemi << "全文重合指数为: " << (float)TotalOfEquals / (float)total << endl;
+
+	if (fabs(0.038 - (float)TotalOfEquals / (float)total)<fabs(0.069 - (float)TotalOfEquals / (float)total)) {
+		cout << "-----理论上支持这是采用多表加密的方法加密的密文" << endl;//单表解密的接近0.069
+		jiemi << "-----理论上支持这是采用多表加密的方法加密的密文" << endl;//单表解密的接近0.069
+	}
+	else {
+		cout << "-----理论上支持这是采用单表加密的方法加密的密文" << endl;
+		jiemi << "-----理论上支持这是采用单表加密的方法加密的密文" << endl;
+	}
+
+	double ratioArray[18];//存放重合指数率的数组
+	double* pratioArray = ratioArray;
+	for (int x = 3; x <= 20; x++) {//分别猜测不同密钥长度下的重合指数
+		float tempfloat = 0;
+		//cout<<"如果密码是"<<x<<"位时,---------------:  "<<endl;
+		double averRatio = 0;
+		for (int n = 0; n<x; n++) {//针对已假设的密码长度进行重合指数计算
+			int index = 0;
+			int tempArray[70000];
+			int* ptempArray = tempArray;
+			int tempTotal = 0;
+
+			for (int m = n; m<Length;) {//这只是一个赋值的过程
+				*(ptempArray++) = *(src + m);
+				tempTotal++;
+				m = m + x;
+			}
+
+			//cout<<"tempTotal="<<tempTotal<<endl;
+			//在这里要一列数据进行重合指数率的计算
+
+			tempfloat = ratio(tempArray, tempTotal);
+			averRatio = averRatio + tempfloat;
+		}
+		*pratioArray++ = (float)averRatio / x;
+	}
+
+	//输出重合指数率
+	for (int h = 3; h <= 20; h++) {
+		cout << "如果密码是" << h << "位时,重合指数率是:  " << (float)ratioArray[h - 3] << endl;
+		jiemi << "如果密码是" << h << "位时,重合指数率是:  " << (float)ratioArray[h - 3] << endl;
+	}
+
+	//分析结构
+	cout << "=================可能密码位数=====================" << endl;
+	jiemi << "=================可能密码位数====================" << endl;
+
+	for (int ma = 0; ma<18; ma++) {
+		if (ratioArray[ma]>0.06) {
+			cout << ma + 3 << "-----" << ratioArray[ma] << endl;
+			jiemi << ma + 3 << "-----" << ratioArray[ma] << endl;
+		}
+	}
+
+	cout << "==============取这些数的公共约数 最可能是密码位数==============" << endl;
+	jiemi << "=============取这些数的公共约数 最可能是密码位数==============" << endl;
+
+	cout << "请输入判断的密码长度:" << endl;
+	jiemi << "请输入判断的密码长度:" << endl;
+	cin >> finalNo;
+	cout << "=====================密码长度为" << finalNo << "时,暴力破解......." << endl;
+	jiemi << "=====================密码长度为" << finalNo << "时,暴力破解......." << endl;
+	char result[4][10];//用于存放预测的结果
+					   //这里假设密码都是小写的英文字符 从97 到122
+
+	for (int mc = 0; mc<finalNo; mc++) {//对密码的每个单位进行破解
+		cout << "==================正在破解第================" << mc + 1 << "位..." << endl;
+		jiemi << "=================正在破解第===============" << mc + 1 << "位..." << endl;
+
+		for (int md = 0; md <= 255; md++) {//这里是从a到z逐个的试探 用function(int c1,int c2)
+			//cout << "---------正在测试----------:" << char(md) << "..." << endl;
+			//jiemi << "---------正在测试----------:" << char(md) << "..." << endl;
+			int mg = 0;
+			int mindex = 0;
+			int mf = 0;
+
+			for (int me = mc; me<Length;) {
+				mf = function(md, (int)*(src + me));
+				if (!(mf == 44 || mf == 46 || mf == 45 || mf == 32 || mf == 33 || mf == 34 || (48 <= mf&&mf <= 59) || (65 <= mf&&mf <= 90) || (97 <= mf&&mf <= 122))) {
+
+					//cout << "临时结果:" << mf << "--" << char(mf) << "不通过" << endl;
+					//jiemi << "临时结果:" << mf << "--" << char(mf) << "不通过" << endl;
+					mg = 1;
+					break;//返回试探下一个
+				}
+				me = me + finalNo;
+			}
+
+			if (mg == 0) {
+				cout << "-----------------------------------------" << char(md) << "--pass!!!" << endl;
+				jiemi << "-----------------------------------------" << char(md) << "--pass!!!" << endl;
+				result[mc][mindex++] = char(md);
+
+			}
+		}
+
+	}
+	jiemi.close();
+}
+#include <set>
+
+void main() {
+
+	const unsigned char test_keys[] = { 0xEB, 0xC2,0xDC,0xC3,0xC4,0xB5,
+		0xD2,0xCA,0xF7,0xD7,0xA3,0xC0,0xE8,0x8F,0xE0,0xCE };
+
+	//ifstream a("a");
+	//ifstream b("b");
+
+	//unsigned char a2, b2;
+	//for (int i = 0; i < 200; i++)
+	//{
+	//	a >> a2;
+	//	b >> b2;
+	//	//int index = i % 16;
+	//	//printf("%c", a2^test_keys[index]);
+	//	printf("%02X", a2^b2);
+	//}
+	FILE* pFile1 = fopen("a", "r");
+	FILE* pFile2 = fopen("b", "r");
+
+	unsigned char buf1[1024] = "";
+	unsigned char buf2[1024] = "";
+	unsigned char buf3[1024] = "";
+
+	fread(&buf1, 200, 1, pFile1);
+	fread(&buf2, 200, 1, pFile2);
+
+	//ofstream ooo("c");
+	unsigned char a1, b1;
+	std::set<unsigned char> keys;
+	for (int i=0;i<78;i++)
+	{
+		unsigned char key = buf1[i] ^ buf2[i];
+		printf("%02X", key);
+		keys.insert(key);
+	}
+	printf("\nkey: %d\n", keys.size());
+	////cout<<function(2,5);
+	//char*src = "There was once a farmer who had a fine olive orchard. He was very hardworking, and the farm always prospered under his care. But he knew that his three sons despised the farm work, and were eager to make wealth, trough adventure.When the farmer was old, and felt that his time had come to die, he called the three sons to him and said, My sons, there is a pot of gold hidden in the olive orchard. Dig for it, if you wish it.After the farmer was dead, the sons went to work to find the pot of gold; since they did not know where the hiding-place was, they agreed to begin in a line, at one end of the orchard, and to dig until one of them should find the money.";
+	//cout << "------------------------明文-----------------------" << endl;
+	//ofstream Minwen("MingWen.txt");
+	//cout << "明文长度:" << my_strlen(src) << endl;
+
+	//for (int iii = 0; iii<my_strlen(src); iii++) {
+	//	cout << (int)*(src + iii) << " ";
+	//	Minwen << (int)*(src + iii) << "  ";
+	//}
+	//Minwen.close();
+	//char*des = new char[2000];
+	//string bb;
+	//cout << endl;
+	//cout << "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^请输入本次加密的key:" << endl;
+	//cin >> bb;
+	//const char*key = bb.c_str();
+	//cout << "lengthofkey=" << my_strlen(key) << endl;
+	//cout << endl;
+	//JiaMi(src, des, key);
+	//cout << endl;
+	//char* des2 = new char[2000];
+	//JieMi(des, des2, key, my_strlen(src));
+	//cout << endl;
+	//int JiMIdes[2000];
+	//ifstream JiaMIin("JiaMi.txt");
+	//int LengthOfMingWen = my_strlen(src);//明文的长度
+	//int k = 0;
+
+	//while (!JiaMIin.eof())
+	//{
+	//	JiaMIin >> JiMIdes[k++];
+	//}
+
+	//测试
+	/*for(int kk=0;kk<LengthOfMingWen;kk++){
+	cout<<JiMIdes[kk]<<" ";
+	}*/
+	char desOfMingWen[200000];
+	FILE* pFile = fopen("735440965", "r");
+	fseek(pFile, 0, SEEK_END); //定位到文件末 
+	int nFileLen = ftell(pFile); //文件长度
+	fseek(pFile, 0, SEEK_SET);
+	char* buffer = new char[nFileLen];
+	fread(buffer, nFileLen, 1, pFile);
+	int* buffer2 = new int[nFileLen];
+	for (int i=0;i<nFileLen;i++)
+	{
+		buffer2[i] = buffer[i];
+	}
+	JieMiWithoutKey(buffer2, desOfMingWen, nFileLen);
+	delete[] buffer;
+}
